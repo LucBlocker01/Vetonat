597bc1cd8b6d65ecc5d127bd0015bf7b7ea2f70b
Chloé BECKER <chloe.becker@etudiant.univ-reims.fr>
2022-11-23 17:51:20 +0100

    ajout du controller de l'Entity Clinique

---
+++ src/Controller/CliniqueController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CliniqueController extends AbstractController
+{
+    #[Route('/clinique', name: 'app_clinique')]
+    public function index(): Response
+    {
+        return $this->render('clinique/index.html.twig', [
+            'controller_name' => 'CliniqueController',
+        ]);
+    }
+}
