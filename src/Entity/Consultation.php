c924ebfa94cd368b1cfb85b68a4c8760c0d390c4
Lucas Hureaux <lucas.hureaux@etudiant.univ-reims.fr>
2022-11-24 15:28:05 +0100

    Création de l'entité Consultation

---
+++ src/Entity/Consultation.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConsultationRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ConsultationRepository::class)]
+class Consultation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $consultationDesc = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateConsultation = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $motifConsultation = null;
+
+    #[ORM\Column]
+    private ?bool $clinique = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getConsultationDesc(): ?string
+    {
+        return $this->consultationDesc;
+    }
+
+    public function setConsultationDesc(string $consultationDesc): self
+    {
+        $this->consultationDesc = $consultationDesc;
+
+        return $this;
+    }
+
+    public function getDateConsultation(): ?\DateTimeInterface
+    {
+        return $this->dateConsultation;
+    }
+
+    public function setDateConsultation(\DateTimeInterface $dateConsultation): self
+    {
+        $this->dateConsultation = $dateConsultation;
+
+        return $this;
+    }
+
+    public function getMotifConsultation(): ?string
+    {
+        return $this->motifConsultation;
+    }
+
+    public function setMotifConsultation(string $motifConsultation): self
+    {
+        $this->motifConsultation = $motifConsultation;
+
+        return $this;
+    }
+
+    public function isClinique(): ?bool
+    {
+        return $this->clinique;
+    }
+
+    public function setClinique(bool $clinique): self
+    {
+        $this->clinique = $clinique;
+
+        return $this;
+    }
+}
