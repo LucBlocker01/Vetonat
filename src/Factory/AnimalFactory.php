dfcd1b310993a412357efe5024540745f844d958
fran0138 <axel.francois@etudiant.univ-reims.fr>
2022-11-29 15:33:15 +0100

    Ajout des fakers pour la génération dans la base de données

--- src/Factory/AnimalFactory.php
+++ src/Factory/AnimalFactory.php
@@ -4,28 +4,28 @@ namespace App\Factory;
 
 use App\Entity\Animal;
 use App\Repository\AnimalRepository;
-use Zenstruck\Foundry\RepositoryProxy;
 use Zenstruck\Foundry\ModelFactory;
 use Zenstruck\Foundry\Proxy;
+use Zenstruck\Foundry\RepositoryProxy;
 
 /**
  * @extends ModelFactory<Animal>
  *
- * @method static Animal|Proxy createOne(array $attributes = [])
- * @method static Animal[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static Animal[]|Proxy[] createSequence(array|callable $sequence)
- * @method static Animal|Proxy find(object|array|mixed $criteria)
- * @method static Animal|Proxy findOrCreate(array $attributes)
- * @method static Animal|Proxy first(string $sortedField = 'id')
- * @method static Animal|Proxy last(string $sortedField = 'id')
- * @method static Animal|Proxy random(array $attributes = [])
- * @method static Animal|Proxy randomOrCreate(array $attributes = [])
- * @method static Animal[]|Proxy[] all()
- * @method static Animal[]|Proxy[] findBy(array $attributes)
- * @method static Animal[]|Proxy[] randomSet(int $number, array $attributes = [])
- * @method static Animal[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
+ * @method static Animal|Proxy                     createOne(array $attributes = [])
+ * @method static Animal[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Animal[]|Proxy[]                 createSequence(array|callable $sequence)
+ * @method static Animal|Proxy                     find(object|array|mixed $criteria)
+ * @method static Animal|Proxy                     findOrCreate(array $attributes)
+ * @method static Animal|Proxy                     first(string $sortedField = 'id')
+ * @method static Animal|Proxy                     last(string $sortedField = 'id')
+ * @method static Animal|Proxy                     random(array $attributes = [])
+ * @method static Animal|Proxy                     randomOrCreate(array $attributes = [])
+ * @method static Animal[]|Proxy[]                 all()
+ * @method static Animal[]|Proxy[]                 findBy(array $attributes)
+ * @method static Animal[]|Proxy[]                 randomSet(int $number, array $attributes = [])
+ * @method static Animal[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
  * @method static AnimalRepository|RepositoryProxy repository()
- * @method Animal|Proxy create(array|callable $attributes = [])
+ * @method        Animal|Proxy                     create(array|callable $attributes = [])
  */
 final class AnimalFactory extends ModelFactory
 {
@@ -38,11 +38,18 @@ final class AnimalFactory extends ModelFactory
 
     protected function getDefaults(): array
     {
+        $lstEspece = [];
+        $EspeceFile = file_get_contents('src/data/Espece.json');
+        $instance = json_decode($EspeceFile, true);
+        foreach ($instance as $element) {
+            $lstEspece[] = $element;
+        }
+
         return [
             // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
-            'nomAnimal' => self::faker()->text(),
-            'EspeceAnimal' => self::faker()->text(),
-            'St�ereliser' => self::faker()->boolean(),
+            'nomAnimal' => self::faker()->name,
+            'EspeceAnimal' => $lstEspece[array_rand($lstEspece)],
+            'Stereliser' => self::faker()->boolean(),
         ];
     }
 
