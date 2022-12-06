af66d099aa66647a1168997ebd0e9e06e6a7de06
fran0138 <axel.francois@etudiant.univ-reims.fr>
2022-12-06 14:00:51 +0100

    Ajout de la fonctionnalité de listage des animaux par clients

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
+use App\Repository\AnimalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnimalController extends AbstractController
 {
-    #[Route('/animal', name: 'app_animal')]
-    public function index(): Response
+    #[Route('/client/{id}', name: 'app_animal')]
+    public function index(AnimalRepository $AnimalRepository, Request $request, Animal $animal): Response
     {
+        // $listContacts = $contactRepository->findBy(array(), array('lastname' => 'ASC', 'firstname' => 'ASC'));
+        $search = $animal->getClient()->getId();
+        if (null == $search) {
+            $search = '';
+        }
+        $listAnimal = $AnimalRepository->search($search);
+
         return $this->render('animal/index.html.twig', [
-            'controller_name' => 'AnimalController',
+            'lstAnimal' => $listAnimal,
         ]);
     }
 }
