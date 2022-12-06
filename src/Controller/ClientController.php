d817ee095df84f9eeafb4eb397aead44e57358e7
Chloé BECKER <chloe.becker@etudiant.univ-reims.fr>
2022-12-06 15:12:33 +0100

    listage des clients

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -3,16 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client')]
-    public function index(): Response
-    {
-        return $this->render('client/index.html.twig', [
-            'controller_name' => 'ClientController',
-        ]);
-    }
 }
