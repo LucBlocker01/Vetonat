f91506a8352adcbc6f6693b27cf63501d0a32986
Enzo Legros <enzo.legros@etudiant.univ-reims.fr>
2022-12-06 16:24:27 +0100

    Template des pages de la navbar

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -8,10 +8,42 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ClientController extends AbstractController
 {
-    #[Route('/client', name: 'app_client')]
+    #[Route('/acceuilClient', name: 'app_client')]
     public function index(): Response
     {
         return $this->render('client/index.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+    #[Route('/acceuilClient/rdv', name: 'app_client_rendezvous')]
+    public function indexRdv(): Response
+    {
+        return $this->render('client/index_rdv.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+    #[Route('/acceuilClient/urgence', name: 'app_client_urgence')]
+    public function indexUrgence(): Response
+    {
+        return $this->render('client/index_urgence.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+    #[Route('/acceuilClient/contact', name: 'app_client_contact')]
+    public function indexContact(): Response
+    {
+        return $this->render('client/index_contact.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+
+    #[Route('/acceuilClient/faq', name: 'app_client_faq')]
+    public function indexFaq(): Response
+    {
+        return $this->render('client/index_faq.html.twig', [
             'controller_name' => 'ClientController',
         ]);
     }
