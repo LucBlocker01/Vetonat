c8804d0ea7fbafee49a061b01ef748119f251c24
fran0124 <tony.franck@etudiant.univ-reims.fr>
2022-12-06 16:03:25 +0100

    Ajout des roles USER pour Clien et ADMIN pour veterinaire

--- src/Factory/ClientFactory.php
+++ src/Factory/ClientFactory.php
@@ -39,7 +39,7 @@ final class ClientFactory extends ModelFactory
     protected function getDefaults(): array
     {
         return [
-            'personne' => PersonneFactory::createOne(),
+            'personne' => PersonneFactory::createOne(['roles' => ['ROLE_USER']]),
         ];
     }
 
