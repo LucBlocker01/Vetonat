eca8cf11717e5698e6e6523708e33331ef644f56
Lucas HUREAUX <lucas.hureaux@etudiant.univ-reims.fr>
2022-12-07 13:44:01 +0100

    Création de la classe ConsultationType

---
+++ src/Form/ConsultationType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Consultation;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ConsultationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('consultationDesc')
+            ->add('dateConsultation')
+            ->add('motifConsultation')
+            ->add('clinique')
+            ->add('urgente')
+            ->add('animal')
+            ->add('traitement')
+            ->add('veterinaire')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Consultation::class,
+        ]);
+    }
+}
