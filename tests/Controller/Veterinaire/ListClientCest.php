2463f28d32cead7d88c96cabd6dc0faeeede3970
Enzo Legros <enzo.legros@etudiant.univ-reims.fr>
2023-01-11 11:53:12 +0100

    Essai ajout test connexion bd ne fonctionne pas

---
+++ tests/Controller/Veterinaire/ListClientCest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Tests\Controller\Veterinaire;
+
+use App\Factory\ClientFactory;
+use App\Factory\PersonneFactory;
+use App\Tests\Support\ControllerTester;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\Security\Factory\RemoteUserFactory;
+
+class  ListClientCest
+{
+    public function testIndexDefault(ControllerTester $I): void
+    {
+        $user = PersonneFactory::createOne ([
+            'roles' => ['ROLE_USER'],
+        ]);
+        $realUser = $user->object();
+        $I->amLoggedInAs($realUser);
+        $I->amOnPage('/veterinaire/clients');
+        $I->seeResponseCodeIs(200);
+        $I->seeInTitle('Vétérinaire - Clients');
+    }
+}
